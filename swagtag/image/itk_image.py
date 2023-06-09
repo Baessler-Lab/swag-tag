96b43de305dcfa8b63399140a740fd9d7eed0e20
laqua-stack <51790423+laqua-stack@users.noreply.github.com>
2023-06-09 19:57:49 +0200

    feat (image): Add ImageBuffer subclass for windowed views of sitk.Image's.
    This class subclasses an sitk.Image and adds a windowing functionality.
    There is a classmethod for creating such an ImageBuffer from an sitk.Image.

--- swagtag/image/itk_image.py
+++ swagtag/image/itk_image.py
@@ -10,14 +10,40 @@ from config.config import dash_conf
 class ImageBuffer(sitk.Image):
     def __init__(
             self, *args,
+            img: sitk.Image,
             window_pct: typing.Tuple[float, float] = None,
             window_abs: typing.Tuple[float, float] = None,
             output_minmax: typing.Tuple[float, float] = (0, 255),
             **kwargs):
-
+        self = ImageBuffer.from_itk(img,
+                                    window_pct=window_pct,
+                                    window_abs=window_abs,
+                                    output_minmax=output_minmax,
+                                    )
+        self.windowed_img: sitk.Image
         super().__init__(*args, **kwargs)
 
-        self.windowed_img = None
+    @property
+    def windowed_img(self) -> sitk.Image:
+        return self.windowed_img
+
+    @windowed_img.setter
+    def windowed_img(self, value):
+        self.windowed_img = value
+
+    @classmethod
+    def cast_from_itk(
+            cls,
+            image: sitk.Image,
+            window_pct: typing.Tuple[float, float] = None,
+            window_abs: typing.Tuple[float, float] = None,
+            output_minmax: typing.Tuple[float, float] = (0, 255),
+    ) -> 'ImageBuffer':
+        self = image
+
+        self.__class__ = ImageBuffer
+        self: ImageBuffer
+
         if window_pct is not None:
             self.window_pct = window_pct
         else:
@@ -42,13 +68,17 @@ class ImageBuffer(sitk.Image):
         # apply windowing
 
         self.apply_window()
-        self.windowed_img: sitk.Image
+        return self
 
     def set_window(
             self,
             window_pct: typing.Tuple[float, float] = None,
-            window_abs: typing.Tuple[float, float] = None
+            window_abs: typing.Tuple[float, float] = None,
+            output_minmax: typing.Tuple[float, float] = None,
     ):
+        if output_minmax is not None:
+            self.output_minmax = output_minmax
+
         if window_abs is not None:
             self.window_abs = window_abs
         else:
@@ -70,10 +100,15 @@ class ImageBuffer(sitk.Image):
             outputMaximum=self.output_minmax[1],
         )
 
-    def windowed(self):
+    def windowed(self) -> sitk.Image:
         return self.windowed_img
 
-    def original(self):
+    def windowed_view_2D_slice(
+            self, slice_no: int = 0,
+    ) -> np.ndarray:
+        return sitk.GetArrayViewFromImage(self.windowed_img[:, :, slice_no])
+
+    def original(self) -> sitk.Image:
         return self
 
 
