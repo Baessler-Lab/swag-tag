26d99e6bcb3283905147a3183c12006bc7ab03ca
laqua-stack <51790423+laqua-stack@users.noreply.github.com>
2023-06-12 00:22:43 +0200

    fix (st_utils): Fix the dataframes in the session_state.
    We load the mapping AccessionNumber <-> StudyInstanceUID into the session state and cache this df.
    We load the image_uris along with some other meta info into a separate df.
    Images are loaded using this df.

--- swagtag/st_utils/states.py
+++ swagtag/st_utils/states.py
@@ -9,9 +9,29 @@ from sql.db_utils import connect_to_db, read_table_to_df
 
 def update_case(case_no: int):
     st.session_state['case_no'] = case_no
-    st.session_state['cur_study_instance_uid'] = st.session_state['map_study_instance_uid_accession_number'][
+    st.session_state['cur_study_instance_uid'] = st.session_state['map_study_instance_uid_accession_number'].iloc[
         st.session_state['case_no']
-    ]
+    ].loc['StudyInstanceUID']
+
+
+@st.cache_data
+def get_mapping_study_instance_uid_to_accession_number() -> pd.DataFrame:
+    return read_table_to_df(
+        table_name=sql_conf['image_uris_table']['table_name'],
+        cols_to_load=['StudyInstanceUID', 'AccessionNumber'],
+        prim_key=sql_conf['image_uris_table']['prim_key'],
+        conn=st.session_state['db_conn'],
+    ).drop_duplicates(inplace=False)
+
+
+@st.cache_data
+def get_image_uris_dataframe() -> pd.DataFrame:
+    return read_table_to_df(
+        table_name=sql_conf['image_uris_table']['table_name'],
+        cols_to_load=['StudyInstanceUID', 'SeriesInstanceUID', 'image_uri'],
+        prim_key=sql_conf['image_uris_table']['prim_key'],
+        conn=st.session_state['db_conn'],
+    ).drop_duplicates(inplace=False)
 
 
 def lookup_accession_number(df: pd.DataFrame, study_instance_uid: str):
@@ -21,16 +41,14 @@ def lookup_accession_number(df: pd.DataFrame, study_instance_uid: str):
 def init_session_states():
     # connect to database
     if 'db_conn' not in st.session_state:
-        st.session_state['db_conn'] = st.cache_resource(ttl=600)(connect_to_db(db_config=db_conf))
+        st.session_state['db_conn'] = connect_to_db(db_config=db_conf)
 
     # load cases
     if 'map_study_instance_uid_accession_number' not in st.session_state:
-        st.session_state['map_study_instance_uid_accession_number'] = st.cache_data()(read_table_to_df(
-            table_name=sql_conf['image_uris_table']['table_name'],
-            cols_to_load=['StudyInstanceUID', 'AccessionNumber'],
-            prim_key=sql_conf['image_uris_table']['prim_key'],
-            conn=st.session_state['db_conn'],
-        ).unique())
+        st.session_state['map_study_instance_uid_accession_number'] = \
+            get_mapping_study_instance_uid_to_accession_number()
+    if 'image_uris_dataframe' not in st.session_state:
+        st.session_state['image_uris_dataframe'] = get_image_uris_dataframe()
 
     # init case iterator
     if 'case_no' not in st.session_state:
@@ -40,14 +58,16 @@ def init_session_states():
     # load images into case iterator
     if 'images' not in st.session_state:
         st.session_state['images'] = load_images_for_study(
-            st.session_state['cur_study_instance_uid'], st.session_state['map_study_instance_uid_accession_number']
+            StudyInstanceUID=st.session_state['cur_study_instance_uid'],
+            df=st.session_state['image_uris_dataframe']
         )
 
     # load reports into case iterator
     if 'report' not in st.session_state:
         st.session_state['report'] = load_report_for_study(
             accession_number=lookup_accession_number(
-                st.session_state['map_study_instance_uid_accession_number']
+                st.session_state['map_study_instance_uid_accession_number'],
+                st.session_state['cur_study_instance_uid'],
             ),
             conn=st.session_state['db_conn']
         )
