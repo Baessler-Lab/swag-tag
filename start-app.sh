9f75ded90b8bdb6672c94d7020af5e7d522f067e
laqua-stack <51790423+laqua-stack@users.noreply.github.com>
2024-01-15 01:29:43 +0100

    chore!: change ports for showcase

--- start-app.sh
+++ start-app.sh
@@ -2,7 +2,7 @@
 
 python -m streamlit run "./swagtag/main.py" \
 --server.fileWatcherType none \
---server.port 8511 \
+--server.port 8510 \
 --server.enableCORS false \
 --server.enableXsrfProtection false \
 --server.enableWebsocketCompression false \
